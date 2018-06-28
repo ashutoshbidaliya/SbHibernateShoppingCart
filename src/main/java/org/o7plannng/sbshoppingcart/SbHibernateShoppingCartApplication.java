74ffb2b439156e65d17bb54893e11b2717745fc3
ashutoshbidaliya <ashutoshbidaliya@gmail.com>
2018-06-28 17:21:37 +0530

    First setup

---
+++ src/main/java/org/o7plannng/sbshoppingcart/SbHibernateShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package org.o7plannng.sbshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernateShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHibernateShoppingCartApplication.class, args);
+	}
+}
